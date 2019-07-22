fec4fd41b58fc2869d932fed1109bc69784ec463
angelasubi <21978301@qq.com>
2019-07-22 14:36:41 +0800

    feat: string

---
+++ string/index.py
@@ -0,0 +1,43 @@
+
+
+print('heelo')
+
+print("he's a mad man")
+
+print('he\'s a mad man')
+
+greet = 'hello'
+
+print(greet[0])
+
+print(greet[1])
+
+print(greet[-1])
+
+print(greet[-5])
+
+print(greet[0:3])       # hel
+
+print(greet[-1:2])      # ''
+
+print(greet[2:-1])      # ll
+
+greet = greet[0:3]
+
+print(greet)
+
+greet = 'hello'
+str2  = 'dudes'
+
+print(greet + ' ' + str2)
+
+print(greet * 3)
+
+greet = greet.upper()
+
+print(greet)
+
+chesses = 'brie, chedder, stilton'
+print(chesses.split(','))
+
+print(len(greet))
